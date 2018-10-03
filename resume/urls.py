a384384a0416827567011e800b1dedfcc59261e3
farnam <homfarnam@gmail.com>
2018-10-03 10:35:33 +0330

    added some files

--- resume/urls.py
+++ resume/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , include , re_path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    path('', views.index, name="index"),
+    re_path('', views.index, name="index"),
+    re_path('blog/',include('blog.urls')),
     
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
